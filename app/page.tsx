b43619e7af3fc2ce12d15a2d562ae5a3662066ca
VidushaSanidu <vidushsanidu07@gmail.com>
2024-05-03 10:19:27 +0530

    nav bar page initialize

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import NavBar from "./components/navbar";
+
 export default function Home() {
-  return <main className="flex min-h-screen flex-col ">Hello world</main>;
+  return (
+    <main className="flex min-h-screen flex-col ">
+      <NavBar />
+    </main>
+  );
 }
