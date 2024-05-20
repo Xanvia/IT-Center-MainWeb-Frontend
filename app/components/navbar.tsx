997f589e72dd70ee9229213a8402c566fc376b7c
VidushaSanidu <vidushsanidu07@gmail.com>
2024-05-20 18:58:03 +0530

    add service and aboutus functions

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,15 +2,22 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { HamButton } from "./buttons/HamButton";
+import { TiArrowSortedDown } from "react-icons/ti";
 
 export default function NavBar() {
+  // dropdown use states
   const [showServicesDropdown, setShowServicesDropdown] =
     useState<boolean>(false);
   const [showAboutDropdown, setShowAboutDropdown] = useState<boolean>(false);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
 
+  // references
+  const servicesDropdownRef = useRef<HTMLDivElement>(null);
+  const aboutDropdownRef = useRef<HTMLDivElement>(null);
+
+  // toggle functions
   const toggleServicesDropdown = () => {
     setShowServicesDropdown(!showServicesDropdown);
     setShowAboutDropdown(false);
@@ -24,6 +31,24 @@ export default function NavBar() {
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
+
+  // unroll dropdowns by clicking outside
+  useEffect(() => {
+    let handler = (e: MouseEvent) => {
+      if (
+        !servicesDropdownRef.current?.contains(e.target as Node) &&
+        !aboutDropdownRef.current?.contains(e.target as Node)
+      ) {
+        setShowServicesDropdown(false);
+        setShowAboutDropdown(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
 
   return (
     <nav className="bg-maroon">
@@ -58,7 +83,11 @@ export default function NavBar() {
 
         {/* right side  */}
         <div className="md:flex items-center">
-          <ul className={`md:flex ${isMobileMenuOpen ? "block" : "hidden"} `}>
+          <ul
+            className={`md:flex items-center ${
+              isMobileMenuOpen ? "block" : "hidden"
+            } `}
+          >
             <li className="mr-6">
               <Link href="/home" className="text-white hover:text-gray">
                 Home
@@ -70,33 +99,40 @@ export default function NavBar() {
               </Link>
             </li>
             <li className="mr-6 relative">
-              <div
-                className="text-white hover:text-gray cursor-pointer"
-                onClick={toggleServicesDropdown}
-              >
-                Services
-              </div>
-              {showServicesDropdown && (
-                <div className="absolute top-full left-0 bg-white p-2 rounded shadow mt-1 text-md">
-                  <ul>
-                    <li className="py-1">
-                      <Link href="/projects" className="text-black">
-                        Projects
-                      </Link>
-                    </li>
-                    <li className="py-1">
-                      <Link href="/consultations" className="text-black">
-                        Consultations
-                      </Link>
-                    </li>
-                    <li className="py-1">
-                      <Link href="/logs" className="text-black">
-                        Logs
-                      </Link>
-                    </li>
-                  </ul>
+              <div ref={servicesDropdownRef}>
+                <div
+                  className="text-white hover:text-gray cursor-pointer flex items-center"
+                  onClick={toggleServicesDropdown}
+                >
+                  Services
+                  <TiArrowSortedDown
+                    className={`w-5 h-5 relative  ${
+                      showServicesDropdown ? "rotate-180 duration-300" : ""
+                    }`}
+                  />
                 </div>
-              )}
+                {showServicesDropdown && (
+                  <div className="absolute top-full left-0 bg-white p-2 rounded shadow mt-1 text-md">
+                    <ul>
+                      <li className="py-1">
+                        <Link href="/projects" className="text-black">
+                          Projects
+                        </Link>
+                      </li>
+                      <li className="py-1">
+                        <Link href="/consultations" className="text-black">
+                          Consultations
+                        </Link>
+                      </li>
+                      <li className="py-1">
+                        <Link href="/logs" className="text-black">
+                          Logs
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
             </li>
             <li className="mr-6">
               <Link href="/news" className="text-white hover:text-gray">
@@ -104,28 +140,35 @@ export default function NavBar() {
               </Link>
             </li>
             <li className="mr-6 relative">
-              <a
-                className="text-white hover:text-gray cursor-pointer"
-                onClick={toggleAboutDropdown}
-              >
-                About Us
-              </a>
-              {showAboutDropdown && (
-                <div className="absolute top-full left-0 bg-white p-2 rounded shadow mt-1">
-                  <ul>
-                    <li className="py-1">
-                      <Link href="/about1" className="text-gray">
-                        Staff
-                      </Link>
-                    </li>
-                    <li className="py-1">
-                      <Link href="/about2" className="text-gray">
-                        Company
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              )}
+              <div ref={aboutDropdownRef}>
+                <a
+                  className="text-white hover:text-gray cursor-pointer flex items-center"
+                  onClick={toggleAboutDropdown}
+                >
+                  About Us
+                  <TiArrowSortedDown
+                    className={`w-5 h-5 relative  ${
+                      showAboutDropdown ? "rotate-180 duration-300" : ""
+                    }`}
+                  />
+                </a>
+                {showAboutDropdown && (
+                  <div className="absolute top-full left-0 bg-white p-2 rounded shadow mt-1">
+                    <ul>
+                      <li className="py-1">
+                        <Link href="/about1" className="text-gray">
+                          Staff
+                        </Link>
+                      </li>
+                      <li className="py-1">
+                        <Link href="/about2" className="text-gray">
+                          Company
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
             </li>
           </ul>
         </div>
