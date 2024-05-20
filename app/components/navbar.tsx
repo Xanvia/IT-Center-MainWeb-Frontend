8cc9c759445bc6cb4d6cf1c695d45da7b47a5854
VidushaSanidu <vidushsanidu07@gmail.com>
2024-05-20 14:59:40 +0530

    setup display properties

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -27,9 +27,9 @@ export default function NavBar() {
 
   return (
     <nav className="bg-maroon">
-      <div className="flex items-center justify-between">
+      <div className="md:flex justify-between">
         {/* left side  */}
-        <div className="flex items-center">
+        <div className="flex items-center justify-between">
           <Link href="/" className="flex items-center m-3">
             <Image
               src="/logo/pera-logo.png"
@@ -45,94 +45,89 @@ export default function NavBar() {
               <h2 className="md:text-base text-sm">University of Peradeniya</h2>
             </div>
           </Link>
-        </div>
-        {/* right side  */}
-        <div className="flex items-center">
-          <div className="flex items-center">
-            <ul
-              className={`md:flex ${
-                isMobileMenuOpen ? "block" : "hidden"
-              } md:block`}
+          {/* hamburger menu button in mobile version */}
+          <div className="md:hidden">
+            <button
+              className="text-white focus:outline-none"
+              onClick={toggleMobileMenu}
             >
-              <li className="mr-5">
-                <Link href="/home" className="text-white hover:text-gray">
-                  Home
-                </Link>
-              </li>
-              <li className="mr-5">
-                <Link href="/courses" className="text-white hover:text-gray">
-                  Courses
-                </Link>
-              </li>
-              <li className="mr-5 relative">
-                <div
-                  className="text-white hover:text-gray cursor-pointer"
-                  onClick={toggleServicesDropdown}
-                >
-                  Services
-                </div>
-                {showServicesDropdown && (
-                  <div className="absolute top-full left-0 bg-white p-2 rounded shadow mt-1 text-md">
-                    <ul>
-                      <li className="py-1">
-                        <Link href="/projects" className="text-black">
-                          Projects
-                        </Link>
-                      </li>
-                      <li className="py-1">
-                        <Link href="/consultations" className="text-black">
-                          Consultations
-                        </Link>
-                      </li>
-                      <li className="py-1">
-                        <Link href="/logs" className="text-black">
-                          Logs
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                )}
-              </li>
-              <li className="mr-4">
-                <Link href="/news" className="text-white hover:text-gray">
-                  News
-                </Link>
-              </li>
-              <li className="mr-5 relative">
-                <a
-                  className="text-white hover:text-gray cursor-pointer"
-                  onClick={toggleAboutDropdown}
-                >
-                  About Us
-                </a>
-                {showAboutDropdown && (
-                  <div className="absolute top-full left-0 bg-white p-2 rounded shadow mt-1">
-                    <ul>
-                      <li className="py-1">
-                        <Link href="/about1" className="text-gray">
-                          Staff
-                        </Link>
-                      </li>
-                      <li className="py-1">
-                        <Link href="/about2" className="text-gray">
-                          Company
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                )}
-              </li>
-            </ul>
-            {/* hamburger menu button  */}
-            <div className="md:hidden">
-              <button
-                className="text-white focus:outline-none"
-                onClick={toggleMobileMenu}
-              >
-                <HamButton isClicked={isMobileMenuOpen} />
-              </button>
-            </div>
+              <HamButton isClicked={isMobileMenuOpen} />
+            </button>
           </div>
+        </div>
+
+        {/* right side  */}
+        <div className="md:flex items-center">
+          <ul className={`md:flex ${isMobileMenuOpen ? "block" : "hidden"} `}>
+            <li className="mr-6">
+              <Link href="/home" className="text-white hover:text-gray">
+                Home
+              </Link>
+            </li>
+            <li className="mr-6">
+              <Link href="/courses" className="text-white hover:text-gray">
+                Courses
+              </Link>
+            </li>
+            <li className="mr-6 relative">
+              <div
+                className="text-white hover:text-gray cursor-pointer"
+                onClick={toggleServicesDropdown}
+              >
+                Services
+              </div>
+              {showServicesDropdown && (
+                <div className="absolute top-full left-0 bg-white p-2 rounded shadow mt-1 text-md">
+                  <ul>
+                    <li className="py-1">
+                      <Link href="/projects" className="text-black">
+                        Projects
+                      </Link>
+                    </li>
+                    <li className="py-1">
+                      <Link href="/consultations" className="text-black">
+                        Consultations
+                      </Link>
+                    </li>
+                    <li className="py-1">
+                      <Link href="/logs" className="text-black">
+                        Logs
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </li>
+            <li className="mr-6">
+              <Link href="/news" className="text-white hover:text-gray">
+                News
+              </Link>
+            </li>
+            <li className="mr-6 relative">
+              <a
+                className="text-white hover:text-gray cursor-pointer"
+                onClick={toggleAboutDropdown}
+              >
+                About Us
+              </a>
+              {showAboutDropdown && (
+                <div className="absolute top-full left-0 bg-white p-2 rounded shadow mt-1">
+                  <ul>
+                    <li className="py-1">
+                      <Link href="/about1" className="text-gray">
+                        Staff
+                      </Link>
+                    </li>
+                    <li className="py-1">
+                      <Link href="/about2" className="text-gray">
+                        Company
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
