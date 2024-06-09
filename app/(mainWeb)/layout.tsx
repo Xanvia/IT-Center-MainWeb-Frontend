02cc3033a37a8fb4464acbdb84c2198ebef3a5af
VidushaSanidu <vidushsanidu07@gmail.com>
2024-06-09 10:55:10 +0530

    Re structured the file system for routing

---
+++ app/(mainWeb)/layout.tsx
@@ -0,0 +1,29 @@
+import { Be_Vietnam_Pro, Rubik } from "next/font/google";
+import NavBar from "../components/sections/navbar";
+
+// font-families
+const vietnam = Be_Vietnam_Pro({
+  weight: "300",
+  subsets: ["latin"],
+  variable: "--font-be-vietnam-pro",
+});
+
+const rubik = Rubik({
+  weight: "500",
+  subsets: ["latin"],
+  variable: "--font-rubik",
+  display: "swap",
+});
+
+export default function MainWebLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className={`${vietnam.variable} ${rubik.variable}`}>
+      <NavBar />
+      {children}
+    </main>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main className=" text-5xl">hello</main>;
+}
