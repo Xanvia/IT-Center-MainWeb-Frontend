d7c5abfd01defcf9787b7bc83bb9f51a6900297b
VidushaSanidu <vidushsanidu07@gmail.com>
2024-06-09 11:50:18 +0530

    add initial code

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,38 @@
+"use client";
+
+import { useState } from "react";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <main>{children}</main>;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  return (
+    <main>
+      <div className="flex h-screen overflow-hidden">
+        {/* Sidebar  */}
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+        {/* <!-- ===== Content Area Start ===== --> */}
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+          {/* <!-- ===== Header Start ===== --> */}
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          {/* <!-- ===== Header End ===== --> */}
+
+          {/* <!-- ===== Main Content Start ===== --> */}
+          <main>
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              {children}
+            </div>
+          </main>
+          {/* <!-- ===== Main Content End ===== --> */}
+        </div>
+        {/* <!-- ===== Content Area End ===== --> */}
+      </div>
+      {/* <!-- ===== Page Wrapper End ===== --> */}
+    </main>
+  );
 }
