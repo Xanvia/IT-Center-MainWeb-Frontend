6dbb004435e001218d4ebe22afb954af9ac1a6af
Bavantha Jayaruwan <jayaruwan2000@gmail.com>
2024-06-16 21:08:50 +0530

    remove 3 pages

--- app/(mainWeb)/about2/Provider.tsx
+++
@@ -1,9 +0,0 @@
-import {NextUIProvider} from "@nextui-org/react";
-
-export default function Provider({children}: { children: React.ReactNode })  {
-  return (
-    <NextUIProvider>
-      {children}
-    </NextUIProvider>
-  );
-}
