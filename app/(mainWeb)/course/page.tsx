559943ff6f7ee33a9bf0b4ad13f343da3bf11e36
Dilshan <tharakadilshan506@gmail.com>
2024-06-22 13:26:37 +0530

    Create course section tsx file

---
+++ app/(mainWeb)/course/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="bg-maroon">
+      {" "}
+      <h1>Course Page</h1>
+    </div>
+  );
+};
+
+export default Home;
