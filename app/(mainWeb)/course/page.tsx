20e5065e304c4f545bb963380a4f97f0e3472dde
Dilshan <tharakadilshan506@gmail.com>
2024-06-24 20:53:24 +0530

    Restructure tabs

--- app/(mainWeb)/course/page.tsx
+++ app/(mainWeb)/course/page.tsx
@@ -3,24 +3,23 @@ import React from "react";
 
 const Home: React.FC = () => {
   return (
-    <div className="bg-white">
-      <div role="tablist" className="tabs tabs-lifted py-10 px-20">
+    <div className="p-10">
+      <h1 className="text-gray-600 font-sans md:text-xl sm:text-lg text-base">
+        Course Overview{" "}
+      </h1>
+      <div role="tablist" className="tabs tabs-lifted">
         <input
           type="radio"
           name="my_tabs_2"
           role="tab"
           className="tab"
-          aria-label="tab1"
+          aria-label="Tab 1"
         />
         <div
           role="tabpanel"
-          className="tab-content bg-gray-300 border-base-300 rounded-box p-6"
+          className="tab-content bg-base-100 border-base-300 rounded-box p-6"
         >
-          <div className="flex sm:flex-row flex-col space-y-4 sm:space-y-0 sm:space-x-5 my-5 justify-center">
-            <HCourseCard />
-            <HCourseCard />
-            <HCourseCard />
-          </div>
+          Tab content 1
         </div>
 
         <input
@@ -28,17 +27,28 @@ const Home: React.FC = () => {
           name="my_tabs_2"
           role="tab"
           className="tab"
-          aria-label="tab2"
+          aria-label="Tab 2"
+          defaultChecked
         />
         <div
           role="tabpanel"
-          className="tab-content bg-gray-300 border-base-300 rounded-box p-6"
+          className="tab-content bg-base-100 border-base-300 rounded-box p-6"
         >
-          <div className="flex sm:flex-row flex-col space-y-4 sm:space-y-0 sm:space-x-5 my-14 justify-center">
-            <HCourseCard />
-            <HCourseCard />
-            <HCourseCard />
-          </div>
+          Tab content 2
+        </div>
+
+        <input
+          type="radio"
+          name="my_tabs_2"
+          role="tab"
+          className="tab"
+          aria-label="Tab 3"
+        />
+        <div
+          role="tabpanel"
+          className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+        >
+          Tab content 3
         </div>
       </div>
     </div>
@@ -46,3 +56,46 @@ const Home: React.FC = () => {
 };
 
 export default Home;
+
+/*
+<div className="mt-5">
+        <div className="font-sans text-base">
+          <div
+            role="tablist"
+            aria-label="tabs"
+            className="tabs tabs-lifted relative w-max grid grid-cols-2 items-center overflow-hidden bg-gray-900/20 shadow-2xl shadow-900/20 rounded-t-xl transition"
+          >
+            <input
+              type="radio"
+              role="tab"
+              aria-selected="true"
+              aria-controls="panel-1"
+              id="tab-1"
+              tabIndex={0}
+              className="relative block tab rounded-t-xl h-10 px-6"
+            >
+              <span className="text-gray-800 font-sans text-bas">
+                IT Center Courses
+              </span>
+            </input>
+            <div
+              role="tabpanel"
+              className="tab-content rounded-b-xl h-40 bg-gray-500"
+            >
+              hello
+            </div>
+            <input
+              type="radio"
+              role="tab"
+              aria-selected="false"
+              aria-controls="panel-2"
+              id="tab-2"
+              tabIndex={-1}
+              className="relative block tab rounded-t-xl-full h-10 px-6"
+            >
+              <span className="text-gray-800 font-sans">OPENED Courses</span>
+            </input>
+          </div>
+        </div>
+      </div>
+ */
