08e6237705b2216e94ff299bd3ec46b01d995f8c
Dilshan <tharakadilshan506@gmail.com>
2024-07-01 22:40:11 +0530

    Make News cards

---
+++ app/(mainWeb)/news/newsCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const NCard = () => {
+  return (
+    <div className="bg-white shadow-xl rounded-lg">
+      <figure>
+        <img
+          className="rounded-t-lg"
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="p-4">
+        <h3 className="text-justify justify-start">
+          IT Center Annual Get-Together 2023
+        </h3>
+        <br />
+        <p className="text-sm text-justify">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore
+          exercitationem totam eligendi odit vitae quis sapiente impedit, quasi
+          id debitis labore soluta iure nisi nulla repellat rerum magni eaque
+          aliquid.
+        </p>
+        <br />
+        <p className="text-end text-xs">Published on 23rd Jun 2024</p>
+      </div>
+    </div>
+  );
+};
+
+/* <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+      </div> */
--- app/(mainWeb)/news/page.tsx
+++ app/(mainWeb)/news/page.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { NCard } from "./newsCard";
 
 const Home: React.FC = () => {
   return (
     <div className="p-10">
-      <h1 className="text-gray-600 font-sans md:text-xl sm:text-lg text-base">
-        News
-      </h1>
+      <h1 className=" font-sans md:text-xl sm:text-lg text-base">News</h1>
       <br />
+      <div className="">
+        <div className="grid lg:grid-cols-3 gap-4 md:grid-cols-2 sm:grid-cols-1">
+          <NCard />
+          <NCard />
+          <NCard />
+          <NCard />
+        </div>
+      </div>
     </div>
   );
 };
