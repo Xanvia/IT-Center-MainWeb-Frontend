a9469457b888b1e0ed96a6b198286fa8bdfe96b4
Dilshan <tharakadilshan506@gmail.com>
2024-07-01 22:09:17 +0530

    Setup news page TSX file

---
+++ app/(mainWeb)/news/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="p-10">
+      <h1 className="text-gray-600 font-sans md:text-xl sm:text-lg text-base">
+        News
+      </h1>
+      <br />
+    </div>
+  );
+};
+
+export default Home;
