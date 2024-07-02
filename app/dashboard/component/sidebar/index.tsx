dedc68db889ead2e59851849ee38da9f0cde5d85
VidushaSanidu <vidushsanidu07@gmail.com>
2024-07-02 17:37:55 +0530

    add logo images

--- app/dashboard/component/sidebar/index.tsx
+++ app/dashboard/component/sidebar/index.tsx
@@ -4,14 +4,34 @@ import Link from "next/link";
 import Image from "next/image";
 
 import {
+  MdNotifications,
   MdOutlineDashboard,
   MdOutlinePieChart,
+  MdOutlineSchool,
+  MdOutlineSettings,
   MdPhonelinkSetup,
 } from "react-icons/md";
 import { HamButton } from "@/app/(mainWeb)/components/buttons/hamButton";
 import SidebarLinkGroup from "./linkGroup";
 import { Tab } from "./tab";
 import { ArrowIcon } from "./arrowIcon";
+import {
+  FaComputer,
+  FaPeopleArrows,
+  FaPeopleGroup,
+  FaRegPaste,
+} from "react-icons/fa6";
+import {
+  BsBuildingFill,
+  BsBuildingFillAdd,
+  BsBuildingFillGear,
+  BsGraphUpArrow,
+} from "react-icons/bs";
+import { FaUserCog } from "react-icons/fa";
+import { PiStudentBold } from "react-icons/pi";
+import { GrUserManager } from "react-icons/gr";
+import { RiAdminFill } from "react-icons/ri";
+import { BiSolidReport } from "react-icons/bi";
 
 interface SidebarProps {
   sidebarOpen: boolean;
@@ -24,10 +44,7 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   const trigger = useRef<any>(null);
   const sidebar = useRef<any>(null);
 
-  let storedSidebarExpanded = "";
-  const [sidebarExpanded, setSidebarExpanded] = useState(
-    storedSidebarExpanded === null ? false : storedSidebarExpanded === "true"
-  );
+  const [sidebarExpanded, setSidebarExpanded] = useState(false);
 
   // close on click outside
   useEffect(() => {
@@ -57,14 +74,14 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
     return () => document.removeEventListener("keydown", keyHandler);
   });
 
-  useEffect(() => {
-    localStorage.setItem("sidebar-expanded", sidebarExpanded.toString());
-    if (sidebarExpanded) {
-      document.querySelector("body")?.classList.add("sidebar-expanded");
-    } else {
-      document.querySelector("body")?.classList.remove("sidebar-expanded");
-    }
-  }, [sidebarExpanded]);
+  // useEffect(() => {
+  //   localStorage.setItem("sidebar-expanded", sidebarExpanded.toString());
+  //   if (sidebarExpanded) {
+  //     document.querySelector("body")?.classList.add("sidebar-expanded");
+  //   } else {
+  //     document.querySelector("body")?.classList.remove("sidebar-expanded");
+  //   }
+  // }, [sidebarExpanded]);
 
   return (
     <aside
@@ -119,7 +136,7 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               <li>
                 <Tab pathname="/courses/registration">
                   <div>
-                    <MdOutlinePieChart size={22} />
+                    <FaRegPaste size={20} />
                   </div>
                   Registration
                 </Tab>
@@ -128,7 +145,7 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               <li>
                 <Tab pathname="/courses">
                   <div>
-                    <MdPhonelinkSetup size={22} />
+                    <MdOutlineSchool size={22} />
                   </div>
                   Enrolled Courses
                 </Tab>
@@ -161,7 +178,7 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               <li>
                 <Tab pathname="/notification">
                   <div>
-                    <MdPhonelinkSetup size={22} />
+                    <MdNotifications size={22} />
                   </div>
                   Notification
                 </Tab>
@@ -170,7 +187,7 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               <li>
                 <Tab pathname="/settings">
                   <div>
-                    <MdPhonelinkSetup size={22} />
+                    <MdOutlineSettings size={22} />
                   </div>
                   Account Settings
                 </Tab>
@@ -203,19 +220,20 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                             : setSidebarExpanded(true);
                         }}
                       >
+                        <BsBuildingFill />
                         Reservations
                         <ArrowIcon open={open} />
                       </div>
                       <div
-                        className={`absolute overflow-hidden dropdown-menu${
-                          open ? "border h-32 opacity-100" : "h-0 opacity-0"
+                        className={` overflow-hidden dropdown-menu  ${
+                          open ? "h-32 opacity-100" : "h-0 opacity-0"
                         }`}
                       >
                         <ul className="mb-5 mt-4 flex flex-col gap-2 pl-6">
                           <li>
                             <Tab pathname="/admin/reservations/requests">
                               <div>
-                                <MdPhonelinkSetup size={22} />
+                                <BsBuildingFillAdd />
                               </div>
                               Requests
                             </Tab>
@@ -223,7 +241,7 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                           <li>
                             <Tab pathname="/admin/reservations/halls">
                               <div>
-                                <MdPhonelinkSetup size={22} />
+                                <BsBuildingFillGear />
                               </div>
                               Labs & Halls
                             </Tab>
@@ -237,19 +255,7 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
 
               <li>
                 <Tab pathname="/admin/courses">
-                  <svg
-                    className="fill-current"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M15.7499 2.9812H14.2874V2.36245C14.2874 2.02495 14.0062 1.71558 13.6405 1.71558C13.2749 1.71558 12.9937 1.99683 12.9937 2.36245V2.9812H4.97803V2.36245C4.97803 2.02495 4.69678 1.71558 4.33115 1.71558C3.96553 1.71558 3.68428 1.99683 3.68428 2.36245V2.9812H2.2499C1.29365 2.9812 0.478027 3.7687 0.478027 4.75308V14.5406C0.478027 15.4968 1.26553 16.3125 2.2499 16.3125H15.7499C16.7062 16.3125 17.5218 15.525 17.5218 14.5406V4.72495C17.5218 3.7687 16.7062 2.9812 15.7499 2.9812ZM1.77178 8.21245H4.1624V10.9968H1.77178V8.21245ZM5.42803 8.21245H8.38115V10.9968H5.42803V8.21245ZM8.38115 12.2625V15.0187H5.42803V12.2625H8.38115ZM9.64678 12.2625H12.5999V15.0187H9.64678V12.2625ZM9.64678 10.9968V8.21245H12.5999V10.9968H9.64678ZM13.8374 8.21245H16.228V10.9968H13.8374V8.21245ZM2.2499 4.24683H3.7124V4.83745C3.7124 5.17495 3.99365 5.48433 4.35928 5.48433C4.7249 5.48433 5.00615 5.20308 5.00615 4.83745V4.24683H13.0499V4.83745C13.0499 5.17495 13.3312 5.48433 13.6968 5.48433C14.0624 5.48433 14.3437 5.20308 14.3437 4.83745V4.24683H15.7499C16.0312 4.24683 16.2562 4.47183 16.2562 4.75308V6.94683H1.77178V4.75308C1.77178 4.47183 1.96865 4.24683 2.2499 4.24683ZM1.77178 14.5125V12.2343H4.1624V14.9906H2.2499C1.96865 15.0187 1.77178 14.7937 1.77178 14.5125ZM15.7499 15.0187H13.8374V12.2625H16.228V14.5406C16.2562 14.7937 16.0312 15.0187 15.7499 15.0187Z"
-                      fill=""
-                    />
-                  </svg>
+                  <MdOutlineSchool size={22} />
                   Courses
                 </Tab>
               </li>
@@ -273,35 +279,7 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                             : setSidebarExpanded(true);
                         }}
                       >
-                        <svg
-                          className="fill-current"
-                          width="18"
-                          height="18"
-                          viewBox="0 0 18 18"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M1.43425 7.5093H2.278C2.44675 7.5093 2.55925 7.3968 2.58737 7.31243L2.98112 6.32805H5.90612L6.27175 7.31243C6.328 7.48118 6.46862 7.5093 6.58112 7.5093H7.453C7.76237 7.48118 7.87487 7.25618 7.76237 7.03118L5.428 1.4343C5.37175 1.26555 5.3155 1.23743 5.14675 1.23743H3.88112C3.76862 1.23743 3.59987 1.29368 3.57175 1.4343L1.153 7.08743C1.0405 7.2843 1.20925 7.5093 1.43425 7.5093ZM4.47175 2.98118L5.3155 5.17493H3.59987L4.47175 2.98118Z"
-                            fill=""
-                          />
-                          <path
-                            d="M10.1249 2.5031H16.8749C17.2124 2.5031 17.5218 2.22185 17.5218 1.85623C17.5218 1.4906 17.2405 1.20935 16.8749 1.20935H10.1249C9.7874 1.20935 9.47803 1.4906 9.47803 1.85623C9.47803 2.22185 9.75928 2.5031 10.1249 2.5031Z"
-                            fill=""
-                          />
-                          <path
-                            d="M16.8749 6.21558H10.1249C9.7874 6.21558 9.47803 6.49683 9.47803 6.86245C9.47803 7.22808 9.75928 7.50933 10.1249 7.50933H16.8749C17.2124 7.50933 17.5218 7.22808 17.5218 6.86245C17.5218 6.49683 17.2124 6.21558 16.8749 6.21558Z"
-                            fill=""
-                          />
-                          <path
-                            d="M16.875 11.1656H1.77187C1.43438 11.1656 1.125 11.4469 1.125 11.8125C1.125 12.1781 1.40625 12.4594 1.77187 12.4594H16.875C17.2125 12.4594 17.5219 12.1781 17.5219 11.8125C17.5219 11.4469 17.2125 11.1656 16.875 11.1656Z"
-                            fill=""
-                          />
-                          <path
-                            d="M16.875 16.1156H1.77187C1.43438 16.1156 1.125 16.3969 1.125 16.7625C1.125 17.1281 1.40625 17.4094 1.77187 17.4094H16.875C17.2125 17.4094 17.5219 17.1281 17.5219 16.7625C17.5219 16.3969 17.2125 16.1156 16.875 16.1156Z"
-                            fill="white"
-                          />
-                        </svg>
+                        <FaPeopleArrows />
                         Services
                         <ArrowIcon open={open} />
                       </div>
@@ -315,7 +293,7 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                           <li>
                             <Tab pathname="/admin/services/log">
                               <div>
-                                <MdPhonelinkSetup size={22} />
+                                <FaPeopleGroup />
                               </div>
                               Logs
                             </Tab>
@@ -323,7 +301,7 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                           <li>
                             <Tab pathname="/admin/services/news">
                               <div>
-                                <MdPhonelinkSetup size={22} />
+                                <FaPeopleGroup />
                               </div>
                               News
                             </Tab>
@@ -331,7 +309,7 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                           <li>
                             <Tab pathname="/admin/services/project">
                               <div>
-                                <MdPhonelinkSetup size={22} />
+                                <FaPeopleGroup />
                               </div>
                               Project
                             </Tab>
@@ -343,25 +321,69 @@ export const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                 }}
               </SidebarLinkGroup>
 
+              <SidebarLinkGroup
+                activeCondition={pathname.includes("/admin/users")}
+              >
+                {(handleClick, open) => {
+                  return (
+                    <>
+                      <div
+                        className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                          pathname.includes("/admin/users") &&
+                          "bg-graydark dark:bg-meta-4"
+                        }`}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          sidebarExpanded
+                            ? handleClick()
+                            : setSidebarExpanded(true);
+                        }}
+                      >
+                        <FaUserCog />
+                        Users
+                        <ArrowIcon open={open} />
+                      </div>
+                      {/* <!-- Dropdown Menu Start --> */}
+                      <div
+                        className={`translate transform overflow-hidden ${
+                          !open && "hidden"
+                        }`}
+                      >
+                        <ul className="mb-5.5 mt-4 flex flex-col gap-2.5 pl-6">
+                          <li>
+                            <Tab pathname="/admin/users/student">
+                              <div>
+                                <PiStudentBold />
+                              </div>
+                              Student
+                            </Tab>
+                          </li>
+                          <li>
+                            <Tab pathname="/admin/users/staff">
+                              <div>
+                                <GrUserManager />
+                              </div>
+                              Staff
+                            </Tab>
+                          </li>
+                          <li>
+                            <Tab pathname="/admin/users/admin">
+                              <div>
+                                <RiAdminFill />
+                              </div>
+                              Admin
+                            </Tab>
+                          </li>
+                        </ul>
+                      </div>
+                    </>
+                  );
+                }}
+              </SidebarLinkGroup>
+
               <li>
                 <Tab pathname="/admin/analysis">
-                  <svg
-                    className="fill-current"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M9.0002 7.79065C11.0814 7.79065 12.7689 6.1594 12.7689 4.1344C12.7689 2.1094 11.0814 0.478149 9.0002 0.478149C6.91895 0.478149 5.23145 2.1094 5.23145 4.1344C5.23145 6.1594 6.91895 7.79065 9.0002 7.79065ZM9.0002 1.7719C10.3783 1.7719 11.5033 2.84065 11.5033 4.16252C11.5033 5.4844 10.3783 6.55315 9.0002 6.55315C7.62207 6.55315 6.49707 5.4844 6.49707 4.16252C6.49707 2.84065 7.62207 1.7719 9.0002 1.7719Z"
-                      fill=""
-                    />
-                    <path
-                      d="M10.8283 9.05627H7.17207C4.16269 9.05627 1.71582 11.5313 1.71582 14.5406V16.875C1.71582 17.2125 1.99707 17.5219 2.3627 17.5219C2.72832 17.5219 3.00957 17.2407 3.00957 16.875V14.5406C3.00957 12.2344 4.89394 10.3219 7.22832 10.3219H10.8564C13.1627 10.3219 15.0752 12.2063 15.0752 14.5406V16.875C15.0752 17.2125 15.3564 17.5219 15.7221 17.5219C16.0877 17.5219 16.3689 17.2407 16.3689 16.875V14.5406C16.2846 11.5313 13.8377 9.05627 10.8283 9.05627Z"
-                      fill=""
-                    />
-                  </svg>
+                  <BiSolidReport />
                   Analysis
                 </Tab>
               </li>
