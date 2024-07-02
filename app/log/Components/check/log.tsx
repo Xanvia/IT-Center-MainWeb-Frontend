83f33a8ecb921e6b3d563ae65bd7b781e941f738
Bavantha Jayaruwan <jayaruwan2000@gmail.com>
2024-07-02 22:36:45 +0530

    recode

--- app/log/Components/check/log.tsx
+++ app/log/Components/check/log.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
           </div>
            
           <div className={styles.SelItem}>
-            <div>
+            <div className={styles.head}>
               <CustomAccordion
                 onItemSelected={setContent}
                 onPhotosSelected={setPhotos}
--- app/log/page.tsx
+++ app/log/page.tsx
@@ -2,9 +2,9 @@ import App from "./Components/check/log"
 
 export default function Home() {
   return (
-  <>
-        <App/>
-  </>
+      <div className='bg-white'>
+            <App/>
+      </div>
 
     
    );
