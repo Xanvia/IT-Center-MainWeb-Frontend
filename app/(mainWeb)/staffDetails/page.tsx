d9a4cc21580adedf5ca3b484a90fef1109ac0b6d
Dilshan <tharakadilshan506@gmail.com>
2024-07-05 16:41:45 +0530

    Add heading

--- app/(mainWeb)/staffDetails/page.tsx
+++ app/(mainWeb)/staffDetails/page.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function contact() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="p-10">
+      <h1 className="text-gray-600 font-sans md:text-xl sm:text-lg text-base">
+        Staff Details
+      </h1>
+      <br />
     </div>
   );
 }
