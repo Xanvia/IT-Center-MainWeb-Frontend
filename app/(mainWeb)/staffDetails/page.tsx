ed66983d464a09e6c9adecb8a53ff55a401c6445
Dilshan <tharakadilshan506@gmail.com>
2024-07-05 17:33:21 +0530

    Add card component

--- app/(mainWeb)/staffDetails/page.tsx
+++ app/(mainWeb)/staffDetails/page.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { StaffCard } from "./staffCard";
 
 export default function contact() {
   return (
     <div className="p-10">
-      <h1 className="text-gray-600 font-sans md:text-xl sm:text-lg text-base">
-        Staff Details
-      </h1>
-      <br />
+      <div className="bg-slate-400">
+        <h2 className="text-gray-600 font-sans md:text-xl sm:text-lg text-base text-center">
+          ADMINISTRATION
+        </h2>
+        <br />
+        <div className="items-center">
+          <StaffCard />
+        </div>
+      </div>
     </div>
   );
 }
