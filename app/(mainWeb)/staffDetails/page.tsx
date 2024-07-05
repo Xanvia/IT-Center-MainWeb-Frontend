17bf8e96b449d4982ed3df7b1d17bd18ed8ef944
Dilshan <tharakadilshan506@gmail.com>
2024-07-05 23:08:26 +0530

    Staff-Administartion complete

--- app/(mainWeb)/staffDetails/page.tsx
+++ app/(mainWeb)/staffDetails/page.tsx
@@ -4,13 +4,22 @@ import { StaffCard } from "./staffCard";
 export default function contact() {
   return (
     <div className="p-10">
-      <div className="bg-slate-400">
-        <h2 className="text-gray-600 font-sans md:text-xl sm:text-lg text-base text-center">
+      <div className="  py-5">
+        <h2 className=" font-sans md:text-xl sm:text-lg text-base text-center">
           ADMINISTRATION
         </h2>
         <br />
-        <div className="items-center">
+        <div className=" flex flex-row justify-center">
           <StaffCard />
+        </div>
+        <div className=" py-10 grid lg:grid-cols-2 gap-5 md:grid-cols-1 ">
+          <div className=" justify-center flex">
+            <StaffCard />
+          </div>
+
+          <div className=" justify-center flex">
+            <StaffCard />
+          </div>
         </div>
       </div>
     </div>
