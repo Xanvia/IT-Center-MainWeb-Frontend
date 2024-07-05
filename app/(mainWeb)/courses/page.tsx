43dfc9fb4d5e460eabe0fac0299e54e2bcff71b9
VidushaSanidu <vidushsanidu07@gmail.com>
2024-07-05 20:56:54 +0530

    course page bug fixed for mobile viewz

--- app/(mainWeb)/courses/page.tsx
+++ app/(mainWeb)/courses/page.tsx
@@ -4,12 +4,12 @@ import HCourseCard from "../components/cards/hCourseCard";
 
 const Home: React.FC = () => {
   return (
-    <div className="p-10">
-      <h1 className=" from-red-700 to-gray-800 bg-clip-text text-transparent bg-gradient-to-t font-bold md:text-3xl sm:text-lg text-center gird">
+    <div className="p-2 md:p-10 overflow-hidden">
+      <h1 className=" from-red-700 to-gray-800 bg-clip-text text-transparent bg-gradient-to-t font-bold md:text-3xl text-xl text-center gird">
         Course Overview
       </h1>
       <div className="flex justify-center mt-1">
-        <div className="bg-yellow-600 h-1 w-36 rounded-md"></div>
+        <div className="bg-yellow-600 h-1 md:w-36 rounded-md"></div>
       </div>
       <br />
 
@@ -24,11 +24,11 @@ const Home: React.FC = () => {
         />
         <div
           role="tabpanel"
-          className="tab-content border-base-300 rounded-box p-6 bg-white"
+          className="tab-content border-base-300 rounded-box p-2 bg-white sm:block hidden"
         >
           <Options />
           <br />
-          <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6 p-5">
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6 sm:m-5">
             <div className="">
               <HCourseCard />
             </div>
