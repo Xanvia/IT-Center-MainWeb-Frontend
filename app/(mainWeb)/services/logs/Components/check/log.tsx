331a455e1f0983d526792946e23b71aaba854fd9
VidushaSanidu <vidushsanidu07@gmail.com>
2024-07-06 13:44:51 +0530

    resstructure file system

--- app/(mainWeb)/services/logs/Components/check/log.tsx
+++ app/(mainWeb)/services/logs/Components/check/log.tsx
@@ -1,13 +1,15 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import { NextUIProvider } from '@nextui-org/react';
-import CustomAccordion from './accordion';
-import styles from '../check/log.module.css';
+import React, { useState } from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import CustomAccordion from "./accordion";
+import styles from "./log.module.css";
 
-const initialContent = 'Item 1';
+const initialContent = "Item 1";
 const initialPhotos = ["/logjpg/im1.jpg", "/logjpg/im2.jpg", "/logjpg/im3.jpg"];
-const initialParagraph = ["1 This is the paragraph for Item 1Some essay samples below are by students who chose to write about a challenge, while other examples may be helpful if you’re looking to write about yourself more generally. And yes, a few of these essays did help these students get accepted into the Ivy League (I’m not telling you which!) though these are all great essays regardless of where (or if) students were admitted to their top choice school."];
+const initialParagraph = [
+  "1 This is the paragraph for Item 1Some essay samples below are by students who chose to write about a challenge, while other examples may be helpful if you’re looking to write about yourself more generally. And yes, a few of these essays did help these students get accepted into the Ivy League (I’m not telling you which!) though these are all great essays regardless of where (or if) students were admitted to their top choice school.",
+];
 
 const App: React.FC = () => {
   const [content, setContent] = useState<string>(initialContent);
@@ -19,9 +21,7 @@ const App: React.FC = () => {
       <div className={styles.Container}>
         <div className={styles.pageName}>Logs</div>
         <div className={styles.ContentWrapper}>
-          <div className={styles.ContentHeadline}>
-            {content}
-          </div>
+          <div className={styles.ContentHeadline}>{content}</div>
 
           <div className={styles.itemBox}>
             <div className={styles.headLine}>
@@ -36,7 +36,12 @@ const App: React.FC = () => {
           <div className={styles.ContentBox}>
             <div className={styles.Photos}>
               {photos.map((photo, index) => (
-                <img className={styles.Photo} key={index} src={photo} alt={`Photo ${index + 1}`} />
+                <img
+                  className={styles.Photo}
+                  key={index}
+                  src={photo}
+                  alt={`Photo ${index + 1}`}
+                />
               ))}
             </div>
             {paragraph && <p className={styles.Paragraph}>{paragraph}</p>}
