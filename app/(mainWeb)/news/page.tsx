27c0a5ba060a47f07047c338d6d23ec09abda1ac
VidushaSanidu <vidushsanidu07@gmail.com>
2024-07-07 00:22:39 +0530

    news page adjustment

--- app/(mainWeb)/news/page.tsx
+++ app/(mainWeb)/news/page.tsx
@@ -4,7 +4,12 @@ import { NCard } from "./newsCard";
 const Home: React.FC = () => {
   return (
     <div className="p-10">
-      <h1 className=" font-sans md:text-xl sm:text-lg text-base">News</h1>
+      <h1 className=" from-red-700 to-gray-800 bg-clip-text text-transparent bg-gradient-to-t font-bold md:text-3xl sm:text-lg text-center gird">
+        News
+      </h1>
+      <div className="flex justify-center mt-1">
+        <div className="bg-yellow-600 h-1 md:w-36 rounded-md"></div>
+      </div>
       <br />
       <div className="pb-10">
         <div className="grid lg:grid-cols-3 gap-4 md:grid-cols-2 sm:grid-cols-1">
