9e60b161bd00144bc93449d7d0c425a8b81425b8
Dilshan <tharakadilshan506@gmail.com>
2024-07-06 13:57:23 +0530

    Remove unwanted codes

--- app/(mainWeb)/news/newsCard.tsx
+++ app/(mainWeb)/news/newsCard.tsx
@@ -27,14 +27,3 @@ export const NCard = () => {
     </div>
   );
 };
-
-/* <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Shoes"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-      </div> */
