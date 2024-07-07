719372c72d1d7bb2cc850f21a372409fcf9d5064
VidushaSanidu <vidushsanidu07@gmail.com>
2024-07-07 23:07:29 +0530

    project structure

---
+++ app/(mainWeb)/services/projects/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import FullSearch from "./components/search/fullSearch";
+import ProPage from "./components/proPage";
+
+export default function page() {
+  return (
+    <div>
+      <FullSearch />
+      <ProPage />
+    </div>
+  );
+}
