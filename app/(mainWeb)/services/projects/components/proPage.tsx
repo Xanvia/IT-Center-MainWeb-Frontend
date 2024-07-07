e9bc11214038505f7c1f4e6cd287c7c7155c6cd0
VidushaSanidu <vidushsanidu07@gmail.com>
2024-07-07 23:10:16 +0530

    complete

--- app/(mainWeb)/services/projects/components/proPage.tsx
+++ app/(mainWeb)/services/projects/components/proPage.tsx
@@ -4,14 +4,15 @@ import CardArray from "./cardArray";
 export default function ProPage() {
   return (
     <div>
-      <h1 className="font-sans md:text-xl sm:text-lg text-base m-10 bg-gray-100" style={{ marginRight: '32px' }}>Projects</h1>
-
-      <div className=" border-2 shadow-lg shadow-gray-500/50 m-8 py-10">
-        
-        <CardArray/>
-  
+      <h1 className=" from-red-700 to-gray-800 bg-clip-text text-transparent bg-gradient-to-t font-bold md:text-3xl sm:text-lg text-center gird">
+        Projects
+      </h1>
+      <div className="flex justify-center mt-1">
+        <div className="bg-yellow-600 h-1 md:w-36 rounded-md"></div>
       </div>
-        
+      <div className=" border-2 shadow-lg shadow-gray-500/50 m-8 py-10">
+        <CardArray />
+      </div>
     </div>
   );
 }
