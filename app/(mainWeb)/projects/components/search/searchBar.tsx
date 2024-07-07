0f2f01d5f378c1a9022df9ebfcf730a97703e4eb
deshan-cyber <deshanaththanayakevf@gmail.com>
2024-07-07 21:20:54 +0530

    change-2

--- app/(mainWeb)/projects/components/search/searchBar.tsx
+++ app/(mainWeb)/projects/components/search/searchBar.tsx
@@ -24,11 +24,11 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
         value={query}
         onChange={handleInputChange}
         placeholder="Search..."
-        className="px-4 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="px-4 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500"
       />
       <button
         onClick={handleSearch}
-        className="px-4 py-2 text-white bg-red-500 shadow-lg shadow-indigo-500/50 rounded-md hover:bg-blue-600"
+        className="px-4 py-2 text-white bg-red-600 shadow-md shadow-red-500/50 rounded-md hover:bg-red-500"
       >
         Search
       </button>
