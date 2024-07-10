5d343d1d5a5968d95af783e04607179e2e0b02c4
VidushaSanidu <vidushsanidu07@gmail.com>
2024-07-10 15:57:40 +0530

    merge

--- app/(mainWeb)/aboutus/aboutUs/page.tsx
+++ app/(mainWeb)/aboutus/aboutUs/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Pictures from "../about2/components/pic/pictures";
 import Directers from "./components/directers/directers";
 import Para from "./components/para/para";
 import Content from "./components/dropdown/content";
@@ -8,7 +7,7 @@ const Home: React.FC = () => {
   return (
     <div className="bg-white ">
       <div className="flex justify-between items-start p-4">
-        <Pictures
+        {/* <Pictures
           imageUrl="/aboutus2img/aboutus.jpg"
           heading="About Us"
           description="Some essay samples below are by students who chose to write about a challenge, 
@@ -16,7 +15,7 @@ const Home: React.FC = () => {
           And yes, a few of these essays did help these students get accepted into the Ivy League, 
           (I’m not telling you which!) though these are all great essays regardless of where (or if) 
           students were admitted to their top choice school."
-        />
+        /> */}
       </div>
       <div className="py-32">
         <Content />
