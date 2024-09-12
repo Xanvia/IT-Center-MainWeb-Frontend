fbf469e97261fa8817949fcff92631bf638bd7ea
VidushaSanidu <vidushsanidu07@gmail.com>
2024-09-12 17:41:51 +0530

    add dashboard landing page UI

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,17 +14,17 @@ export default function Home() {
     <main className="">
       <h1 className="font-rubik text-5xl my-5 text-gray-600">Good Morning!</h1>
       {/* Primary shortcuts */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 my-8">
         {primaryShortcuts.map((shortcut) => (
           <Link key={shortcut.name} href={shortcut.href} passHref>
-            <Card isPressable className="w-full h-full">
-              <CardBody className="p-0">
-                <Image
-                  src={shortcut.visual}
-                  alt={`${shortcut.name} illustration`}
-                  className="w-full object-cover h-40"
-                />
-              </CardBody>
+            <Card isPressable className="border-none w-full h-full">
+              <Image
+                src={shortcut.visual}
+                alt={`${shortcut.name} illustration`}
+                className="object-cover object-center contrast-75 brightness-90 w-full h-40"
+                height={400}
+                width={400}
+              />
               <CardFooter className="flex-col items-center p-4">
                 <shortcut.icon className="w-8 h-8 mb-2" />
                 <p className="text-lg font-semibold">{shortcut.name}</p>
@@ -35,19 +35,15 @@ export default function Home() {
       </div>
 
       {/* Secondary shortcuts */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4">
         {secondaryShortcuts.map((shortcut) => (
           <Link key={shortcut.name} href={shortcut.href} passHref>
             <Card isPressable className="w-full h-full">
-              <CardBody className="p-0">
-                <Image
-                  src={shortcut.visual}
-                  alt={`${shortcut.name} illustration`}
-                  className="w-full object-cover h-24"
-                />
+              <CardBody className="items-center my-2">
+                <shortcut.icon size={64} />
               </CardBody>
+
               <CardFooter className="flex-col items-center p-2">
-                <shortcut.icon className="w-6 h-6 mb-1" />
                 <p className="text-sm text-center">{shortcut.name}</p>
               </CardFooter>
             </Card>
