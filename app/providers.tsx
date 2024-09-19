29031288409b9c0e809ec1a3c3e6d90eb7acf1e9
VidushaSanidu <vidushsanidu07@gmail.com>
2024-09-19 22:42:28 +0530

    add session provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,10 @@
 import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <SessionProvider>{children}</SessionProvider>
+    </NextUIProvider>
+  );
 }
