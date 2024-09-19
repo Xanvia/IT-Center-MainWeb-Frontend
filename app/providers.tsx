6fcf6b0e4c5d5a4c5596c14e84213750c9f4a459
VidushaSanidu <vidushsanidu07@gmail.com>
2024-09-19 22:54:24 +0530

    add layout provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,5 @@
 import { NextUIProvider } from "@nextui-org/react";
-import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <NextUIProvider>
-      <SessionProvider>{children}</SessionProvider>
-    </NextUIProvider>
-  );
+  return <NextUIProvider>{children}</NextUIProvider>;
 }
