83c22f9956b36ff85814c2475523d529085cb4f8
VidushaSanidu <vidushsanidu07@gmail.com>
2024-09-23 07:58:37 +0530

    auth done

--- app/actions/auth.tsx
+++
@@ -1,7 +0,0 @@
-// "use server";
-
-// import { registerSchemaType } from "../(system)/auth/components/schema/registerFormSchema";
-
-// export async function signup(data: registerSchemaType) {
-
-// }
