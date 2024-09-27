5d30486fe826f34a21ce00d8a82c2dec701b0a4e
Dilshan <tharakadilshan506@gmail.com>
2024-09-27 09:01:22 +0530

    staff schema changed

--- schemas/staffSchema.ts
+++ schemas/staffSchema.ts
@@ -3,26 +3,19 @@ import { z } from "zod";
 const phoneRegex = /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/;
 
 export const staffSchema = z.object({
-  title: z.string().min(1, "Title is required"),
+  title: z.string().toUpperCase().min(1, "Title is required"),
   displayName: z.string().min(1, "Display name is required"),
-  displayDesignation: z.string().min(1, "Display designation is required"),
+  designation: z.string().min(1, "Display designation is required"),
   nominal: z.string().min(1, "Nominal is required"),
-  extNumber: z.string().min(1, "Extension number is required"),
+  extNo: z.string().min(1, "Extension number is required"),
 
   emails: z
-    .array(
-      z.object({
-        value: z.string().email("Invalid email address"),
-      })
-    )
-    .min(1, "At least one email is required"),
-  phoneNumbers: z
-    .array(
-      z.object({
-        value: z.string().regex(phoneRegex, "Invalid phone number"),
-      })
-    )
-    .min(1, "At least one phone number is required"),
+  .array(z.string().email("Invalid email address"))
+  .min(1, "At least one email is required"),
+
+telephones: z
+  .array(z.string().regex(phoneRegex, "Invalid phone number"))
+  .min(1, "At least one phone number is required")
 });
 
 export type StaffFormData = z.infer<typeof staffSchema>;
