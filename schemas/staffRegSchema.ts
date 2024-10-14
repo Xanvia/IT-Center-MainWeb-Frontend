9598dbe5c6d85396b9b9bf8d515450240411be54
Dilshan <tharakadilshan506@gmail.com>
2024-10-14 23:58:58 +0530

    update staff schema

--- schemas/staffRegSchema.ts
+++ schemas/staffRegSchema.ts
@@ -1,20 +1,25 @@
-import { z } from "zod";
+import * as z from "zod";
 
 export const staffRegSchema = z.object({
-  title: z.string().min(1, { message: "Title is required" }),
   displayName: z.string().min(1, { message: "Display name is required" }),
   designation: z.string().min(1, { message: "Designation is required" }),
   nominal: z.string().min(1, { message: "Nominal is required" }),
   extNo: z.string().min(1, { message: "Extension number is required" }),
   emails: z
     .array(
-      z.string().email({ message: "Invalid email address" })
+      z.object({
+        value: z.string().email({ message: "Invalid email address" }),
+      })
     )
     .min(1, { message: "At least one email is required" })
     .max(2, { message: "You can provide up to two email addresses" }),
   telephones: z
     .array(
-      z.string().regex(/^\d{10}$/, { message: "Invalid phone number" })
+      z.object({
+        value: z
+          .string()
+          .regex(/^\d{10}$/, { message: "Invalid phone number" }),
+      })
     )
     .min(1, { message: "At least one telephone number is required" })
     .max(2, { message: "You can provide up to two telephone numbers" }),
