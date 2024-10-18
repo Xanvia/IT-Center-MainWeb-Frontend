6d0f9e6434b4f89913d8f3ed3545ebd312246941
Dilshan <tharakadilshan506@gmail.com>
2024-10-18 23:06:01 +0530

    Minor Changes

--- app/(system)/dashboard/courseRegistration/courseCard.tsx
+++ app/(system)/dashboard/courseRegistration/courseCard.tsx
@@ -6,6 +6,7 @@ interface CourseCardProps {
   image: string;
   code: string;
   name: string;
+  isClickable?: boolean;
 }
 
 export default function CourseCard({ image, code, name }: CourseCardProps) {
