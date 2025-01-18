aa8bc990ea8c1a6e6e5b50e7fe586024e8a9a854
Dilshan <tharakadilshan506@gmail.com>
2025-01-18 14:22:40 +0530

    edjust course card to display the neccesary details

--- app/(system)/dashboard/courseRegistration/courseCard.tsx
+++ app/(system)/dashboard/courseRegistration/courseCard.tsx
@@ -1,23 +1,45 @@
 // card.tsx
 
 import { Card, CardHeader, CardBody } from "@nextui-org/react";
+import { Banknote, Calendar, DollarSign, Users } from "lucide-react";
 
 interface CourseCardProps {
   id: string;
   image: string;
-  code: string;
-  name: string;
+  courseCode: string;
+  courseName: string;
+  registrationDeadline: string;
+  fees: number;
 }
 
-export default function CourseCard({ image, code, name }: CourseCardProps) {
+export default function CourseCard({
+  image,
+  courseCode: code,
+  courseName: name,
+  registrationDeadline: regDL,
+  fees,
+}: CourseCardProps) {
   return (
-    <Card>
+    <Card className="rounded-lg">
       <CardHeader className="p-2">
-        <img src={image} alt={name} className="w-full object-cover" />
+        <img
+          src={image}
+          alt={name}
+          className="w-full object-cover rounded-sm"
+        />
       </CardHeader>
       <CardBody>
         <h2 className="text-sm font-semibold">{code}</h2>
         <p className="text-md">{name}</p>
+
+        <div className="flex items-center pt-2 space-x-2 text-gray-600">
+          <Banknote className="h-5 w-5" />
+          <span>Rs. {fees}</span>
+        </div>
+        <div className="flex items-center space-x-2 text-gray-600">
+          <Calendar className="h-5 w-5" />
+          <span>RegDL : {regDL}</span>
+        </div>
       </CardBody>
     </Card>
   );
