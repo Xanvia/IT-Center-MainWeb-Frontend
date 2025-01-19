d4acbe69da5f595c03118e7f3a435bf8a7f71270
Dilshan <tharakadilshan506@gmail.com>
2025-01-19 20:59:54 +0530

    add yello pay button varieant

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,6 +19,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        yellow:
+          "bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded",
       },
       size: {
         default: "h-9 px-4 py-2",
