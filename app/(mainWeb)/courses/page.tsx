f768755ac7c2b0e75469b85e03c474e08aa2a3bd
Dilshan <tharakadilshan506@gmail.com>
2025-01-19 10:33:02 +0530

    update course interface with updated schema

--- app/(mainWeb)/courses/page.tsx
+++ app/(mainWeb)/courses/page.tsx
@@ -3,17 +3,21 @@
 import { useState, useMemo, Key } from "react";
 import { Link, Input } from "@nextui-org/react";
 import { Search } from "lucide-react";
-import { courses } from "@/app/(system)/dashboard/courseRegistration/courseData";
-import CourseCard from "@/app/(system)/dashboard/courseRegistration/courseCard";
+import { courses } from "@/app/(system)/dashboard/courseRegistration/newCourseData";
+import CourseCard from "./courseCardMain";
 
 export default function CourseRegistration() {
   const [searchQuery, setSearchQuery] = useState("");
 
-  const filteredCourses = useMemo(() => {
+  {
+    /* 
+    const filteredCourses = useMemo(() => {
     return courses.filter((course: { name: string }) =>
       course.name.toLowerCase().includes(searchQuery.toLowerCase())
     );
   }, [searchQuery]);
+    */
+  }
 
   return (
     <div className="container mx-auto px-28 py-8">
@@ -34,28 +38,21 @@ export default function CourseRegistration() {
         </div>
 
         <div className="align-middle grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {filteredCourses.map(
-            (course: {
-              id: Key | null | undefined;
-              image: string;
-              code: string;
-              name: string;
-            }) => (
-              <Link
-                href={`/dashboard/courseRegistration/${course.id}`}
-                key={course.id}
-              >
-                <div className=" max-w-sm rounded-lg border border-gray-200 hover:shadow-md transition-shadow duration-300">
-                  <CourseCard
-                    image={course.image}
-                    code={course.code}
-                    name={course.name}
-                    id={""}
-                  />
-                </div>
-              </Link>
-            )
-          )}
+          {courses.map((course) => (
+            <Link
+              href={`/dashboard/courseRegistration/${course.id}`}
+              key={course.id}
+            >
+              <div className=" max-w-sm rounded-lg border border-gray-200 hover:shadow-md transition-shadow duration-300">
+                <CourseCard
+                  image={course.images[0]}
+                  courseCode={course.courseCode}
+                  courseName={course.courseName}
+                  id={""}
+                />
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
