ddf20c76e6be6b209e9495592831bfcc80674ec7
Dilshan <tharakadilshan506@gmail.com>
2025-01-21 17:25:17 +0530

    add neccesary actual footer links

--- app/(mainWeb)/components/sections/footer.tsx
+++ app/(mainWeb)/components/sections/footer.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import SocialMedia from "../footerContent/socialMedia";
 import Contact from "../footerContent/contact";
 import Image from "next/image";
+import Link from "next/link";
+import {
+  footerServicesLinks,
+  footerLinksTypes,
+  footerOurLinks,
+  footerOtherLinks,
+} from "@/constants/mainLinksData";
 
 const Footer = () => {
   return (
@@ -35,24 +42,27 @@ const Footer = () => {
         <div className="flex justify-center px-5 md:px-0 footer pt-5 md:w-1/3 text-gray-400">
           <nav className="w-36 md:mx-3">
             <h6 className="footer-title text-yellow-300">Our Services</h6>
-            <a className="link link-hover">Projects</a>
-            <a className="link link-hover">Consultations</a>
-            <a className="link link-hover">Logs</a>
-            <a className="link link-hover">News</a>
+            {footerServicesLinks.map((item: footerLinksTypes) => (
+              <Link href={item.url} className="overflow-hidden">
+                {item.id}
+              </Link>
+            ))}
           </nav>
           <nav className="w-48 md:mx-3 ">
             <h6 className="footer-title text-yellow-300">Our Links</h6>
-            <a className="link link-hover">Reservation</a>
-            <a className="link link-hover">O P E N E D</a>
-            <a className="link link-hover">Course Registrations</a>
-            <a className="link link-hover">Staff Details</a>
+            {footerOurLinks.map((item: footerLinksTypes) => (
+              <Link href={item.url} className="overflow-hidden">
+                {item.id}
+              </Link>
+            ))}
           </nav>
           <nav className="w-44 md:mx-3 ">
             <h6 className="footer-title text-yellow-300">Other Links</h6>
-            <a className="link link-hover">web Mail</a>
-            <a className="link link-hover">University of Peradeniya</a>
-            {/* <a className="link link-hover"></a>
-            <a className="link link-hover">Press kit</a> */}
+            {footerOtherLinks.map((item: footerLinksTypes) => (
+              <Link href={item.url} className="overflow-hidden">
+                {item.id}
+              </Link>
+            ))}
           </nav>
         </div>
         <Contact />
